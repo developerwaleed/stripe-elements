ecd80c6ba6b23a0ef13c416511ce54629f8bdd7c
Waleed Amjad <waleedamjad99@hotmail.com>
2024-03-21 14:05:49 +0500

    Refactor code to improve performance and readability

--- app/checkout/payment-form.tsx
+++ app/checkout/payment-form.tsx
@@ -1,118 +1,144 @@
+"use client";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { useState, useEffect } from "react";
+import { toast } from "sonner";
+
 export const PaymentForm = () => {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent?.status) {
+        case "succeeded":
+          toast.success("Payment succeeded!");
+          break;
+        case "processing":
+          toast.loading("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          toast.error("Your payment was not successful, please try again.");
+          break;
+        default:
+          toast.error("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js hasn't yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
+      },
+    });
+
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      toast.error(error.message);
+      //   setMessage(error.message ? error.message : "");
+    } else {
+      toast.error("An unexpected error occurred.");
+    }
+
+    setIsLoading(false);
+  };
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
   return (
     <div className="col-span-full py-6 px-4 sm:py-12 lg:col-span-6 lg:py-24">
       <div className="mx-auto w-full max-w-lg">
-        <h1 className="relative text-2xl font-medium text-gray-700 sm:text-3xl">
+        <h1 className="relative text-2xl font-medium text-gray-700 sm:text-3xl pb-8">
           Secure Checkout
           <span className="mt-2 block h-1 w-10 bg-teal-600 sm:w-20"></span>
         </h1>
-        <form action="" className="mt-10 flex flex-col space-y-4">
-          <div>
-            <label
-              htmlFor="email"
-              className="text-xs font-semibold text-gray-500"
+
+        <form id="payment-form" onSubmit={handleSubmit}>
+          <PaymentElement
+            id="payment-element"
+            options={paymentElementOptions}
+          />
+          <p className="mt-10 text-center text-sm font-semibold text-gray-500">
+            By placing this order you agree to the{" "}
+            <a
+              href="#"
+              className="whitespace-nowrap text-teal-400 underline hover:text-teal-600"
             >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="john.capler@fang.com"
-              className="mt-1 block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-            />
-          </div>
-          <div className="relative">
-            <label
-              htmlFor="card-number"
-              className="text-xs font-semibold text-gray-500"
-            >
-              Card number
-            </label>
-            <input
-              type="text"
-              id="card-number"
-              name="card-number"
-              placeholder="1234-5678-XXXX-XXXX"
-              className="block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 pr-10 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-            />
-            <img
-              src="/images/uQUFIfCYVYcLK0qVJF5Yw.png"
-              alt=""
-              className="absolute bottom-3 right-3 max-h-4"
-            />
-          </div>
-          <div>
-            <p className="text-xs font-semibold text-gray-500">
-              Expiration date
-            </p>
-            <div className="mr-6 flex flex-wrap">
-              <div className="my-1">
-                <label htmlFor="month" className="sr-only">
-                  Select expiration month
-                </label>
-                <select
-                  name="month"
-                  id="month"
-                  className="cursor-pointer rounded border-gray-300 bg-gray-50 py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                >
-                  <option value="">Month</option>
-                </select>
-              </div>
-              <div className="my-1 ml-3 mr-6">
-                <label htmlFor="year" className="sr-only">
-                  Select expiration year
-                </label>
-                <select
-                  name="year"
-                  id="year"
-                  className="cursor-pointer rounded border-gray-300 bg-gray-50 py-3 px-2 text-sm shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                >
-                  <option value="">Year</option>
-                </select>
-              </div>
-              <div className="relative my-1">
-                <label htmlFor="security-code" className="sr-only">
-                  Security code
-                </label>
-                <input
-                  type="text"
-                  id="security-code"
-                  name="security-code"
-                  placeholder="Security code"
-                  className="block w-36 rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <label htmlFor="card-name" className="sr-only">
-              Card name
-            </label>
-            <input
-              type="text"
-              id="card-name"
-              name="card-name"
-              placeholder="Name on the card"
-              className="mt-1 block w-full rounded border-gray-300 bg-gray-50 py-3 px-4 text-sm placeholder-gray-300 shadow-sm outline-none transition focus:ring-2 focus:ring-teal-500"
-            />
-          </div>
-        </form>
-        <p className="mt-10 text-center text-sm font-semibold text-gray-500">
-          By placing this order you agree to the{" "}
-          <a
-            href="#"
-            className="whitespace-nowrap text-teal-400 underline hover:text-teal-600"
+              Terms and Conditions
+            </a>
+          </p>
+          <button
+            className="mt-4 inline-flex w-full items-center justify-center rounded bg-teal-600 py-2.5 px-4 text-base font-semibold tracking-wide text-white text-opacity-80 outline-none ring-offset-2 transition hover:text-opacity-100 focus:ring-2 focus:ring-teal-500 sm:text-lg"
+            disabled={isLoading || !stripe || !elements}
+            id="submit"
           >
-            Terms and Conditions
-          </a>
-        </p>
-        <button
-          type="submit"
-          className="mt-4 inline-flex w-full items-center justify-center rounded bg-teal-600 py-2.5 px-4 text-base font-semibold tracking-wide text-white text-opacity-80 outline-none ring-offset-2 transition hover:text-opacity-100 focus:ring-2 focus:ring-teal-500 sm:text-lg"
-        >
-          Place Order
-        </button>
+            <span id="button-text">
+              {isLoading ? (
+                <div className="spinner" id="spinner"></div>
+              ) : (
+                "Pay now"
+              )}
+            </span>
+          </button>
+        </form>
       </div>
     </div>
   );
